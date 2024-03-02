4d9ffad9687d709f2e9db42999b34b23f019d50b
fjdjgncfsh <156096178+fjdjgncfsh@users.noreply.github.com>
2024-03-02 19:43:45 +0800

    Update MainActivity.kt

--- src/android/app/src/main/java/org/yuzu/yuzu_emu/ui/main/MainActivity.kt
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/ui/main/MainActivity.kt
@@ -57,6 +57,7 @@ import java.io.FileOutputStream
 import java.io.IOException
 import androidx.appcompat.app.AlertDialog
 import android.content.Context
+import android.app.Dialog
 import androidx.fragment.app.DialogFragment
 
 class MainActivity : AppCompatActivity(), ThemeProvider {
