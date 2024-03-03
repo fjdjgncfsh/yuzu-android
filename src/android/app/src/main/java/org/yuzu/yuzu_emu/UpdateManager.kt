bcbfed3354f0404385390db5cbc062e1bfc3ea59
fjdjgncfsh <156096178+fjdjgncfsh@users.noreply.github.com>
2024-03-03 19:34:45 +0800

    Update UpdateManager.kt

--- src/android/app/src/main/java/org/yuzu/yuzu_emu/UpdateManager.kt
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/UpdateManager.kt
@@ -3,7 +3,6 @@ package org.yuzu.yuzu_emu
 import android.content.Context
 import android.content.Intent
 import android.os.Environment
-import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.FileProvider
 import kotlinx.coroutines.Dispatchers
